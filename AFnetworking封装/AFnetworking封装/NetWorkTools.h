0bd1825952ece053fd51583aa3d9332d3788da66
xiyuwuhen <15201732653@163.com>
2016-10-20 14:42:48 +0800

    1111

--- AFnetworking封装/AFnetworking封装/NetWorkTools.h
+++ AFnetworking封装/AFnetworking封装/NetWorkTools.h
@@ -9,13 +9,19 @@
 #import "AFHTTPSessionManager.h"
 #import "AFNetworking.h"
 
-//typedef void (^afnetFinishedCallBack) (NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) ;
+typedef NS_ENUM(NSInteger,RequestType) {
+    RequestTypeGet,
+    RequestTypePost
+};
 @interface NetWorkTools : AFHTTPSessionManager
+
 /**
  *  创建当前类的一个单例
  *  
  */
 + (instancetype)sharedInstance;
 
--(void)request:(NSString *)urlString parameters:(NSMutableDictionary*)parameters finished:(void(^)(id  responseObject , NSError* error))afnetFinishedCallBack ;
+-(void)requestMethodType:(RequestType)RequestType urlString:(NSString *)urlString parameters:(NSMutableDictionary*)parameters finished:(void(^)(id  responseObject , NSError* error))afnetFinishedCallBack ;
+//func loadAccessToken(code : String, finished : (result : [String : AnyObject]?, error : NSError?) -> ())
+-(void)loadAccessToken:(NSString *)code finish:(void(^)(NSDictionary *result,NSError *error ))finished;
 @end
--- AFnetworking封装/AFnetworking封装/NetWorkTools.m
+++ AFnetworking封装/AFnetworking封装/NetWorkTools.m
@@ -9,23 +9,37 @@
 #import "NetWorkTools.h"
 
 static NetWorkTools *shareObject;
+typedef void (^successCallBack)(NSURLSessionDataTask * _Nonnull, id _Nullable) ;
 @implementation NetWorkTools
 + (instancetype)sharedInstance
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         shareObject = [[NetWorkTools alloc]init];
-        
+        shareObject.responseSerializer.acceptableContentTypes =[NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html",@"text/plain", nil];
     });
     return shareObject;
 }
 
--(void)request:(NSString *)urlString parameters:(NSMutableDictionary*)parameters finished:(void(^)(id  responseObject , NSError* error))afnetFinishedCallBack    {
-    [self GET:urlString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        afnetFinishedCallBack(responseObject,nil);
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        afnetFinishedCallBack(nil , error);
-    }];
+-(void)requestMethodType:(RequestType)RequestType urlString:(NSString *)urlString parameters:(NSMutableDictionary*)parameters finished:(void(^)(id  responseObject , NSError* error))afnetFinishedCallBack    {
+    
+    
+    if (RequestType == RequestTypeGet) {
+        [self GET:urlString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            afnetFinishedCallBack(responseObject,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            afnetFinishedCallBack(nil , error);
+        }];
+    }else{
+        [self POST:urlString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            afnetFinishedCallBack(responseObject,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             afnetFinishedCallBack(nil , error);
+        }];
+    }
+    
     
 }
+
+
 @end
